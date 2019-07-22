b53c5ad4e442f1d5484a91f472b616b62196c0f3
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-22 11:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  const token = 'dc39f8ac3be84e0fc077eeb7f2265f347a22c120';
-const postData = {
-  body: 'Great stuff'
-};
- 
-fetch(
-  'https://api.github.com/repos/AtesSuToprak/learn-co-sandbox/commits/:sha/comments',
-  {
-    method: 'POST',
-    body: JSON.stringify(postData),
-    headers: {
-      Authorization: `token ${token}`
-    }
-  }
-).then(res => console.log(res));
   return '';
 }
 
